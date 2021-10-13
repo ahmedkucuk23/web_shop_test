68c9270627602c4faa776588c97748345759e473
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-10-13 15:47:21 +0200

    ahmo

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,4 @@ Rails.configuration.stripe = {
   secret_key:      ENV['STRIPE_SECRET_KEY']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key =  sk_test_51Jc7vFBehfIfznsuglUimC5L6TwQoz39TvoZfYaRjnkqRQT7iqnIJlYXU7cAqbzIf77VvS54YZipj9pKgSv2hFHw0048fYukU2
